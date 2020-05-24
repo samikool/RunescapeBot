d6e3c1524c099d9b88a35d5c34fc1f983ccc8be3
Sam <sam.morgan44@gmail.com>
2020-05-23 22:27:21 -0500

    initial commit

---
+++ src/master.py
@@ -0,0 +1,15 @@
+class master:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    
+    def getVal(self):
+        return self.x
+
+    ##Master.start() function will start the bot network
+    def start(self):
+        pass
+    
+    ##startBot function will start a bot for the network
+    def startBot(self):
+        pass
---
+++ src/slave.py
@@ -0,0 +1,6 @@
+class slave:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def getVal(self):
+        return self.x
