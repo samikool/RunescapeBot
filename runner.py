d62824f6ec660350e150e7da0358b5d3fb44bc1a
Sam <sam.morgan44@gmail.com>
2020-05-26 01:55:35 -0500

    added creating virtual displays and starting a vnc server functinality

--- runner.py
+++ runner.py
@@ -1,15 +1,26 @@
 import sys
 import os
-if(os.name == 'nt'):
-    import win32.win32gui as screen
-elif(os.name == 'posix'):
-    from gi.repository import wnck
-
-
+import subprocess
 from time import sleep
 
+numBots = sys.argv[1]
 
-print(os.name)
+def killOldBots():
+    print('Killing old bots')
+    subprocess.call('killall X')
+def createDisplays(numDisplays):
+    print('Creating '+ numDisplays + ' displays for the bots...')
 
-while(True):
-    sleep(3)
+    subprocess.call(['./createNewDisplay.sh', str(numBots)])
+    
+    print(numDisplays + ' bots created.')
+
+def startVNC(displayNum):
+    print('Starting VNC for '+ str(displayNum) +'...')
+
+    subprocess.call(['./startVNC.sh', str(displayNum)])
+    
+    print('VNC for '+ str(displayNum) + ' created.')
+
+
+createDisplays(numBots)
